9f4845d295cc2f5133b8228a137e98c9c9ddf72c
Rituraj Jain <rrjain20122000@gmail.com>
2020-11-18 10:09:12 +0530

    Create Chatlist.java

---
+++ app/src/main/java/com/lotpick/lotpick/Models/Chatlist.java
@@ -0,0 +1,22 @@
+package com.lotpick.lotpick.Models;
+
+public class Chatlist {
+
+    public String id;
+
+    public Chatlist(String id) {
+        this.id = id;
+    }
+
+    public Chatlist() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
